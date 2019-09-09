53cf58c630139a06a7ddab7dccb49757062d38d6
Stefan Wille <github@stefanwille.com>
2019-09-09 21:39:17 +0200

    Test

--- src/Store.test.ts
+++ src/Store.test.ts
@@ -2,5 +2,21 @@ import App from "./App";
 import Store from "./Store";
 
 describe("Store", () => {
-  it("renders without crashing", () => {});
+  it("todoCount", () => {
+    const store = new Store();
+    expect(store.todoCount).toBe(3);
+    store.addTodo({ done: false, text: "Test the store" });
+    expect(store.todoCount).toBe(4);
+    store.deleteTodo(0);
+    expect(store.todoCount).toBe(3);
+  });
+
+  it("openTodos", () => {
+    const store = new Store();
+    expect(store.openTodos.length).toBe(3);
+    store.todos.forEach(todo => {
+      todo.done = true;
+    });
+    expect(store.openTodos.length).toBe(0);
+  });
 });
