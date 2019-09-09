a3914dc59e2c5172d42e9b7a2b7b3c7a4cd80740
Stefan Wille <github@stefanwille.com>
2019-09-09 21:40:24 +0200

    .

--- src/Store.ts
+++ src/Store.ts
@@ -1,41 +1,41 @@
-import { configure, observable, action, computed, decorate } from 'mobx';
+import { configure, observable, action, computed, decorate } from "mobx";
 
-configure({ enforceActions: 'observed' });
+configure({ enforceActions: "observed" });
 
 interface Todo {
-    done: boolean;
-    text: string;
+  done: boolean;
+  text: string;
 }
 
 class Store {
-    todos: Todo[] = [ 'Buy milk', 'Write book', 'Sleep' ].map((text) => ({
-        done: false,
-        text
-    }));
+  todos: Todo[] = ["Buy milk", "Write book", "Sleep"].map(text => ({
+    done: false,
+    text
+  }));
 
-    get todoCount() {
-        return this.todos.length;
-    }
+  get todoCount() {
+    return this.todos.length;
+  }
 
-    get openTodos() {
-        return this.todos.filter((todo) => !todo.done);
-    }
+  get openTodos() {
+    return this.todos.filter(todo => !todo.done);
+  }
 
-    addTodo(todo: Todo) {
-        this.todos.push(todo);
-    }
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
 
-    deleteTodo(index: number) {
-        this.todos.splice(index, 1);
-    }
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
 
 decorate(Store, {
-    todos: observable,
-    todoCount: computed,
-    openTodos: computed,
-    addTodo: action,
-    deleteTodo: action
+  todos: observable,
+  todoCount: computed,
+  openTodos: computed,
+  addTodo: action,
+  deleteTodo: action
 });
 
 export default Store;
